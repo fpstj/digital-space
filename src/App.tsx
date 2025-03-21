1b05445a283b42315fba104858b5b16fea97e6a9
Filip Stojanov <fs@seveniteen.com>
2025-03-21 13:56:18 +0100

    Updates to backgrounds

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,9 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import MainLayout from "./layouts/MainLayout";
 import Home from "./pages/Home";
+import Projects from "./pages/Projects";
+import Contact from "./pages/Contact";
+import About from "./pages/About";
 
 function App() {
   // const [count, setCount] = useState(0)
@@ -10,9 +13,9 @@ function App() {
       <Routes>
         <Route path="/" element={<MainLayout />}>
           <Route index element={<Home />} />
-          {/* <Route path="about" element={<About />} /> */}
-          {/* <Route path="projects" element={<Projects />} /> */}
-          {/* <Route path="contact" element={<Contact />} /> */}
+          <Route path="projects" element={<Projects />} />
+          <Route path="about" element={<About />} />
+          <Route path="contact" element={<Contact />} />
         </Route>
       </Routes>
     </Router>
--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -4,20 +4,25 @@ import "./Navbar.css";
 
 const Navbar: React.FC = () => {
   return (
-    <nav className="flex bg-black/70 p-4 w-full backdrop-blur-md fixed gap-5 left-0 top-0">
-      {["Home", "About", "Projects", "Contact"].map((item) => (
-        <NavLink
-          key={item}
-          to={`/${item.toLowerCase()}`}
-          className={({ isActive }) =>
-            `relative text-white text-lg pb-1 transition-all ${
-              isActive ? "after:w-full" : "after:w-0"
-            } after:block after:h-[2px] after:bg-pink-500 after:absolute after:bottom-[-2px] after:left-0 after:transition-all after:duration-300`
-          }
-        >
-          {item}
-        </NavLink>
-      ))}
+    <nav className="flex items-center place-content-center bg-black/10 p-4 w-full backdrop-blur-md fixed gap-10 left-0 top-0">
+      {["home", "projects", "about", "contact"].map((item) => {
+        const path = item === "home" ? "/" : `/${item.toLowerCase()}`;
+        return (
+          <NavLink
+            key={item}
+            to={path}
+            className={({ isActive }) =>
+              `relative text-lg font-normal pb-1 transition-all after:block after:h-[.2rem] after:bg-pink-600 after:absolute after:bottom-[-2px] after:left-0 after:transition-all after:duration-300 ${
+                isActive
+                  ? "text-pink-600 font-semibold after:w-full"
+                  : "text-stone hover:text-pink-500 hover:font-medium after:w-0"
+              }`
+            }
+          >
+            {item}
+          </NavLink>
+        );
+      })}
     </nav>
   );
 };
